3b55093f5a81f779d42829ced96bc776816d8c5d
Fabricius97 <christoffer.fabricius@hotmail.com>
2023-03-29 17:25:23 +0200

    Made arrow img to a checkbox and style the faqs

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+// const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+// const texts = document.querySelectorAll('span[id^="spans"]');
+
+
+// for (let i = 0; i < checkboxes.length; i++) {
+//   checkboxes[i].addEventListener('change', function() {
+//     const parentP = this.parentElement;
+//     const parentDiv = parentP.parentElement;
+//     const textSpan = parentDiv.querySelector('span[id^="spans"]');
+//     if (this.checked) {
+//       textSpan.style.display = 'block';
+//       parentP.style.fontWeight = 'bold';
+//     } else {
+//       textSpan.style.display = 'none';
+//       parentP.style.fontWeight = 'normal';
+//     }
+//   });
+// }
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const texts = document.querySelectorAll('span[id^="spans"]');
+
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener('change', function() {
+    const parentP = this.parentElement;
+    const parentDiv = parentP.parentElement;
+    const textSpan = parentDiv.querySelector('span[id^="spans"]');
+    const arrowImg = parentDiv.querySelector('img[id^="arrowImg"]');
+    if (this.checked) {
+      textSpan.style.display = 'block';
+      parentP.style.fontWeight = 'bold';
+      arrowImg.classList.add('rotated');
+    } else {
+      textSpan.style.display = 'none';
+      parentP.style.fontWeight = 'normal';
+      arrowImg.classList.remove('rotated');
+    }
+  });
+}
+
